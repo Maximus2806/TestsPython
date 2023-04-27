a378a04f5616dd6b9cc46fb3039fd4dc0c6b3a7e
Maximus2806 <marozaumaksim@gmail.com>
2023-04-28 00:11:31 +0300

    Project settings

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+from config.url import Url
+from config.playwright import Playwright
+from config.expectetions import Expectations
+
+url = Url()
+playwright = Playwright()
+expectations = Expectations()
+
+
